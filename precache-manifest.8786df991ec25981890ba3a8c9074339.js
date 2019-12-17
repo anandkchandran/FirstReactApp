8e9e53a4a5cbff85bd295caa50f6baf90148d2e0
anandkchandran <anandkchandran@yahoo.com>
2019-12-17 18:58:26 +0530

    Updates

---
+++ precache-manifest.8786df991ec25981890ba3a8c9074339.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0ece134587d012f73cc08eef84ee03ac",
+    "url": "/FirstReactApp/index.html"
+  },
+  {
+    "revision": "d543639e14be9e11fbeb",
+    "url": "/FirstReactApp/static/css/main.54d84da1.chunk.css"
+  },
+  {
+    "revision": "4ee796cfabedc5bd953d",
+    "url": "/FirstReactApp/static/js/2.cd5ddd3b.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/FirstReactApp/static/js/2.cd5ddd3b.chunk.js.LICENSE"
+  },
+  {
+    "revision": "d543639e14be9e11fbeb",
+    "url": "/FirstReactApp/static/js/main.39010f0e.chunk.js"
+  },
+  {
+    "revision": "4bf7f1fb4c691b862002",
+    "url": "/FirstReactApp/static/js/runtime-main.d35149c6.js"
+  }
+]);
